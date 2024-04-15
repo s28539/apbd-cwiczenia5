da6c25c4211084e0835c84a37d7e2efb368a5144
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-04-15 23:27:45 +0200

    Change Animal structure

--- Tutorial4/Database/StaticData.cs
+++ Tutorial4/Database/StaticData.cs
@@ -6,17 +6,37 @@ public class StaticData
 {
     public static List<Animal> animals = new List<Animal>()
     {
-        new Animal(1, "Rex", PetCategory.Dog, 30.0, KnownColor.SaddleBrown),
-        new Animal(2, "Whiskers", PetCategory.Cat, 5.0, KnownColor.Silver),
-        new Animal(3, "Tweety", PetCategory.Bird, 0.5, KnownColor.Yellow),
-        new Animal(4, "Goldie", PetCategory.Fish, 0.1, KnownColor.Gold),
-        new Animal(5, "Bunny", PetCategory.Rabbit, 4.0, KnownColor.Wheat),
-        new Animal(6, "Shadow", PetCategory.Cat, 4.5, KnownColor.Gray),
-        new Animal(7, "Fido", PetCategory.Dog, 20.0, KnownColor.SteelBlue),
-        new Animal(8, "Blub", PetCategory.Fish, 0.2, KnownColor.Teal),
-        new Animal(9, "Flappy", PetCategory.Bird, 0.3, KnownColor.MediumOrchid),
-        new Animal(10, "Snuggles", PetCategory.Rabbit, 3.5, KnownColor.PeachPuff),
-        new Animal(11, "Paws", PetCategory.Dog, 22.5, KnownColor.OliveDrab),
-        new Animal(12, "Splash", PetCategory.Fish, 0.4, KnownColor.SteelBlue)
+        new Animal(1, "Rex", "Dog", 30.0, "SaddleBrown"),
+        new Animal(2, "Whiskers", "Cat", 5.0, "Silver"),
+        new Animal(3, "Tweety", "Bird", 0.5, "Yellow"),
+        new Animal(4, "Goldie", "Fish", 0.1, "Gold"),
+        new Animal(5, "Bunny", "Rabbit", 4.0, "Wheat"),
+        new Animal(6, "Shadow", "Cat", 4.5, "Gray"),
+        new Animal(7, "Fido", "Dog", 20.0, "SteelBlue"),
+        new Animal(8, "Blub", "Fish", 0.2, "Teal"),
+        new Animal(9, "Flappy", "Bird", 0.3, "MediumOrchid"),
+        new Animal(10, "Snuggles", "Rabbit", 3.5, "PeachPuff"),
+        new Animal(11, "Paws", "Dog", 22.5, "OliveDrab"),
+        new Animal(12, "Splash", "Fish", 0.4, "SteelBlue")
     };
+    public static void DeleteAnimal(int id)
+    {
+        foreach (var animal in animals)
+        {
+            if (animal.Id == id)
+            {
+                animals.Remove(animal);
+                break;
+            }
+        }
+        // for (int i = 0; i < animals.Capacity; i++)
+        // {
+        //     if (animals[i].Id == id)
+        //     {
+        //         animals.Remove(animals[i]);
+        //         break;
+        //     }
+        // }
+
+    }
 }
