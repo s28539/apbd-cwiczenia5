3135549188860b9292c70ccce6c448e9136ee4d4
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-04-15 22:49:58 +0200

    add fields of Animal.cs

--- Tutorial4/Models/Animal.cs
+++ Tutorial4/Models/Animal.cs
@@ -1,7 +1,21 @@
-﻿namespace Tutorial4;
+﻿using System.Drawing;
+
+namespace Tutorial4;
 
 public class Animal
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public PetCategory Category { get; set; }
+    public double weight { get; set; }
+    public KnownColor Color { get; set; }
+
+    public Animal(int id, string name, PetCategory category, double weight, KnownColor color)
+    {
+        Id = id;
+        Name = name;
+        Category = category;
+        this.weight = weight;
+        Color = color;
+    }
 }
